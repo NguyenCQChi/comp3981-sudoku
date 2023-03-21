ef30d6604f6eebaf2c372e079dab95f7ef05ad7d
Sarah Semkow <ssemkow@my.bcit.ca>
2023-03-21 13:52:20 -0700

    Altered CSP to be a bit more working

--- src/utils/csp.ts
+++ src/utils/csp.ts
@@ -2,14 +2,27 @@ const CSP = (board: any) => {
   // console.log("solving csp")
   const boardSize= board.length  
   let csp: never[] = []
+  let queue = getAllArcs(board)
   let unassignedCells = getAllUnassigned(board)
-  // unassignedCells.forEach((row, col) => {
-  //   backtrack(csp, board)
-  // });
-  console.log(board)
-  backtrack(csp, board)
+  // console.log(board)
+  
+  backtrack(csp, board, queue)
   // console.log(board)
 };
+
+const getAllArcs = (board: any) => {
+  let queue: any[] = []
+  // queue[0][1] = [XiRow, XiCol, XjRow, XjCol]
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      let emptyNeighborsQueue: any[] = []
+      let neighborsQueue: any[] = getNeighbors(board, emptyNeighborsQueue, i, j)
+      queue = queue.concat(neighborsQueue)      
+    }
+  }
+  return queue
+}
 
 const getAllUnassigned = (board: any) => {
   let allUnassignedCells = []
@@ -26,9 +39,7 @@ const getAllUnassigned = (board: any) => {
 //n = size of board, c = 1-size of board (all the choices)
 const isValid = (board: any, row: number, col: number, n: number, c: any) => {
   const smallBoxRow = Math.floor(Math.sqrt(n)) //3 flipped
-  // console.log(smallBoxRow)
   const smallBoxCol = Math.ceil(Math.sqrt(n)) //4
-  // console.log(smallBoxCol)
   const blockRow = Math.floor(row / smallBoxRow) * smallBoxRow;
   const blockCol = Math.floor(col / smallBoxCol) * smallBoxCol;
   for (let i = 0; i < n; i++) {
@@ -40,15 +51,12 @@ const isValid = (board: any, row: number, col: number, n: number, c: any) => {
   return true;
 }
 
-function backtrack(csp: any, board: any) {
+function backtrack(csp: any, board: any, queue: any) {
   const completed = isBoardComplete(board)
-  // console.log("completed variable")
   if (completed) {
-    return board
+    return true
   }
-  // console.log("getting cell domains")
   var boardDomains = getCellDomains(csp, board)
-  // console.log(boardDomains)
 
   // find which variable (cell) should be assigned next
   var unassignedCell = selectUnassignedVariable(csp, board, boardDomains)
@@ -58,26 +66,38 @@ function backtrack(csp: any, board: any) {
   // order the values for which the variable should be tried (ie 1 or 9 or 5 or etc)
   var orderOfDomainValues = orderDomainValues(csp, unassignedCell, board, boardDomains) //map  
   var sortedDomainValues = sortOrderOfDomainValues(orderOfDomainValues)
+  // console.log("sortedDomainValues")
+  // sortedDomainValues.forEach((x)=> console.log(x))
 
+  //domainValue is not used, we just needed key in the mah {key=1:2}
   sortedDomainValues.forEach((domainValue,key) => {
     if (isValid(board, row, col, board.length, key)) {
+      // console.log("BEFORE: Assigning " + key + " to row " + row + ", col " + col)
+      // console.log("board domains before:")
+      // boardDomains.forEach((d) => console.log(d))
       board[row][col] = key
-      var currentCellOldDomains = boardDomains[row][col]//note: might need to use a different copy
-      boardDomains[row][col] = [key]
+      var currentCellOldDomains = boardDomains[row][col]//note: currently shallow copy, might need a deep copy
+      
       console.log("Assigning " + key + " to row " + row + ", col " + col)
-      let inferences = inference(csp, unassignedCell, board, boardDomains)
-      console.log("inferences " + inferences)
+      boardDomains[row][col] = []
+      boardDomains[row][col].push(key)
+      // console.log("key: " + key)
+      // console.log("board domains of recently assigned cell: " + boardDomains[row][col])
+      // console.log("board domains after")
+      // boardDomains.forEach((d) => console.log(d))
+      let inferences = inference(csp, board, boardDomains, queue)
+      // console.log("inferences " + inferences)
   
       if (inferences) {
-        // console.log("inferences is true")
         csp.push(inferences)
-        let result = backtrack(csp, board)
-        console.log(result) //Check if a value has been added
+        let result = backtrack(csp, board, queue)
+        // console.log(result) //Check if a value has been added
         // if result does not equal failure, return result
-        if (isBoardComplete(result)) {//result != board
+        if (result) {//result != board
+          // return result
           return result
         }
-        csp.pop(inferences)
+        csp.pop(inferences)//!!!!!! needs to be fixed
       }
       board[row][col] = 0
       let currentCellNewDomain = boardDomains[row][col]
@@ -86,8 +106,8 @@ function backtrack(csp: any, board: any) {
       boardDomains[row][col].splice(index, 1)
     }
   });
-  // console.log(board)
-  return board
+  // return board
+  return false
 }
 
 const getNeighbors = (board: any, queue: any, row: any, col: any) => {
@@ -121,34 +141,21 @@ const getNeighbors = (board: any, queue: any, row: any, col: any) => {
   return queue
 }
 
-// const inference = (csp: any, unassignedCell: any, board: any, boardDomains: any) => {
-//   let row = unassignedCell[0]
-//   let col = unassignedCell[1]
-
-// }
-
 // AC-3 Algorithm
-const inference = (csp: any, unassignedCell: any, board: any, boardDomains: any) => {
+const inference = (csp: any, board: any, boardDomains: any, queue: any) => {
   // queue of initial arcs
   let initialQueue: never[] = []
-  let row = unassignedCell[0]
-  let col = unassignedCell[1]
 
-  let queue = getNeighbors(board, initialQueue, row, col)
-  // console.log("initial queue")
-  // console.log(typeof(queue))
-  // console.log(queue)
-
+  // console.log(queue.length)
   while (queue.length != 0) {
-    // console.log("queue in while loop")
-    // queue.forEach((q: any) => console.log(q));
     let popped = queue.shift() //removes first item from queue
     if (popped != undefined) {
       var XiRow = popped[0]
       var XiCol = popped[1]
     }
     if (revise(csp, popped, boardDomains)) {
-      console.log("XiRow: " + XiRow + " XiCol: " + XiCol + " num domains: " + boardDomains[XiRow][XiCol].length)
+      
+      // console.log("XiRow: " + XiRow + " XiCol: " + XiCol + " num domains: " + boardDomains[XiRow][XiCol].length)
       
       // if (board[XiRow][XiCol] == 0 && boardDomains[XiRow][XiCol].length == 0) {
       if (boardDomains[XiRow][XiCol].length == 0) {
@@ -176,20 +183,23 @@ const revise = (csp: any, popped: any, boardDomains: any) => {
   let XjRow = popped[2]
   let XjCol = popped[3]
   let satisfiesConstraint = true
-  console.log("board domains")
-  console.log(boardDomains)
-  // console.log("popped")
-  // console.log(popped)
+  // console.log("in revise")
+
   boardDomains[XiRow][XiCol].forEach((x: any) => {
     // if no value in Xj's domain allows (Di, Dj) to satisfy the constraint b/w Xi and Xj
-    // Xi's domain = {1, 2, 3}, Xj's domain = {1, 2, 3}
-    if (!boardDomains[XjRow][XjCol].includes(x) && boardDomains[XjRow][XjCol].length == 1) { //Di {1,2,3} Dj{1,2,3,4}
+    // Xi's domain = {1, 2, 3}, Xj's domain = {3}
+    // console.log("length: " + boardDomains[XjRow][XjCol].length)
+    // console.log(boardDomains[XjRow][XjCol])
+    if (boardDomains[XjRow][XjCol].includes(x) && boardDomains[XjRow][XjCol].length == 1) { //Di {1,2,3} Dj{1,2,3,4}
+      // console.log(boardDomains[XjRow][XjCol])
+      // console.log(x)
+      // console.log("reducing domain")
+      console.log(boardDomains[XiRow][XiCol])
       let index = boardDomains[XiRow][XiCol].indexOf(x)
       boardDomains[XiRow][XiCol].splice(index, 1)
+      console.log(boardDomains[XiRow][XiCol])
       revised = true
     }
-
-
     // boardDomains[XjRow][XjCol].forEach((Dj: any) => {
     //   // console.log("Dj: " + Dj + " Di: " + Di)
     //   if (Dj == Di) {
@@ -211,21 +221,17 @@ const revise = (csp: any, popped: any, boardDomains: any) => {
 //sorts domain values from lowest to highest
 const sortOrderOfDomainValues = (orderDomainValues: any) => {
   const sorted = new Map([...orderDomainValues.entries()].sort((a, b) => a[1] - b[1]))
+  // console.log("sorted order of domain values")
+  // console.log(sorted)
   return sorted
 }
 
 const orderDomainValues = (csp: any, unassignedCell: any, board: any, boardDomains: any) => {
   let row = unassignedCell[0]
   let col = unassignedCell[1]
-  // console.log("row: " + unassignedCell[0])
-  // console.log("col: " + unassignedCell[1])
 
   let sortedDomainValues = new Map()
-  // console.log(boardDomains)
   boardDomains[row][col].forEach((d: any) => sortedDomainValues.set(d, 0)) //ex. [1:0, 2:0, 3:0, 8:0]
-
-  // console.log("initial domain values of first cell")
-  // console.log(sortedDomainValues)
 
   for (let i = 0; i < board.length; i++) {
     if (board[row][i] == 0 && i != col) {
@@ -251,8 +257,6 @@ const orderDomainValues = (csp: any, unassignedCell: any, board: any, boardDomai
       }
     }
   }
-  // console.log("sorted domain val")
-  // console.log(sortedDomainValues)
   return sortedDomainValues
 }
 
@@ -328,7 +332,6 @@ const getDegree = (board: any, row: any, col: any) => {
       }
     }
   }
-  // console.log(numAffectedDomains)
   return numAffectedDomains.length
 }
 
@@ -356,9 +359,6 @@ const reduceDomain = (board: any, row: number, col: number) => {
   const numbersInRow = getNumbersInSameRow(board, row)
   const numbersInCol = getNumbersInSameCol(board, col)
   const numbersInSameBox = getNumbersInSameBox(board, row, col)
-  // console.log("num in row " + numbersInRow)
-  // console.log("num in col " + numbersInCol)
-  // console.log("numbers in same box " + numbersInSameBox)
 
   numbersInRow.forEach((n) => updatedDomain.add(n))
   numbersInCol.forEach((n) => updatedDomain.add(n))
